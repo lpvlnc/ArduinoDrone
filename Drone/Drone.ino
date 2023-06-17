d8080bfc67d8575e3c0b26acd468c885813ff2db
Leonardo Valença <lpvalenca@hotmail.com>
2023-06-17 15:52:02 -0300

    Code that receive the data for each of the escs

--- Drone/Drone.ino
+++ Drone/Drone.ino
@@ -1,50 +1,75 @@
-#include <SoftwareSerial.h>
-#include <Servo.h>
-
 #define pinESC1 8
 #define pinESC2 9
 #define pinESC3 10
 #define pinESC4 11
-
-SoftwareSerial HC05(0, 1);
+#include <Servo.h>
+#include <SoftwareSerial.h>
 
 Servo esc1;
 Servo esc2;
 Servo esc3;
 Servo esc4;
 
+SoftwareSerial AT09(0, 1);
+
 int minSpeed = 1000;
 int maxSpeed = 2000;
 int speed = minSpeed;
+int firstSpeed = minSpeed;
+int secondSpeed = minSpeed;
+int thirdSpeed = minSpeed;
+int fourthSpeed = minSpeed;
+bool started = false;
+int specialSpeed = minSpeed;
+char speedsArray[] = "1000,1000,1000,1000";
 
 void setup() {
-  Serial.begin(9600);
-  HC05.begin(9600);
+  Serial.begin(250000);
+  AT09.begin(9600);
   initializeESC();
 }
 
 void loop() {
   readBluetooth();
-  setSpeed();
+  if (started)
+  {
+    updateEscSpeed();
+  }
 }
 
 void readBluetooth()
 {
-  if (HC05.available() > 0)
+  if (AT09.available() > 0)
   {
-    int message = HC05.readString().toInt();
-    messageHandler(message);
+    String data = AT09.readString();
+    Serial.print(data);
+
+    char buf[sizeof(speedsArray)];
+    data.toCharArray(buf, sizeof(buf));
+    char *p = buf;
+    char *str;
+    int i = 1;
+    while((str = strtok_r(p, ",", &p)) != NULL)
+    {
+      if (i == 1)
+        firstSpeed = atoi(str);
+
+      if (i == 2)
+        secondSpeed = atoi(str);
+
+      if (i == 3)
+        thirdSpeed = atoi(str);
+
+      if (i == 4)
+        fourthSpeed = atoi(str);
+      i++;
+    }
+
+    // Serial.println(firstSpeed);
+    // Serial.println(secondSpeed);
+    // Serial.println(thirdSpeed);
+    // Serial.println(fourthSpeed);
   }
-}
-
-void messageHandler(int message)
-{
-  speed = message;
-  if (speed < minSpeed)
-    speed = minSpeed;
-
-  if (speed > maxSpeed)
-    speed = maxSpeed;
 }
 
 void initializeESC()
@@ -70,13 +95,38 @@ void initializeESC()
   esc4.writeMicroseconds(minSpeed);
 
   delay(5000);
+  started = true;
 }
 
 
-void setSpeed()
+void updateEscSpeed()
 {
-  esc1.writeMicroseconds(speed);
-  esc2.writeMicroseconds(speed);
-  esc3.writeMicroseconds(speed);
-  esc4.writeMicroseconds(speed);
+  NormalizeSpeeds();
+  esc1.writeMicroseconds(firstSpeed);
+  esc2.writeMicroseconds(secondSpeed);
+  esc3.writeMicroseconds(thirdSpeed);
+  esc4.writeMicroseconds(fourthSpeed);
+}
+
+void NormalizeSpeeds()
+{
+  if (firstSpeed < minSpeed)
+    firstSpeed = minSpeed;
+  if (firstSpeed > maxSpeed)
+    firstSpeed = maxSpeed;
+
+  if (secondSpeed < minSpeed)
+    secondSpeed = minSpeed;
+  if (secondSpeed > maxSpeed)
+    secondSpeed = maxSpeed;
+
+  if (thirdSpeed < minSpeed)
+    thirdSpeed = minSpeed;
+  if (thirdSpeed > maxSpeed)
+    thirdSpeed = maxSpeed;
+
+  if (fourthSpeed < minSpeed)
+    fourthSpeed = minSpeed;
+  if (fourthSpeed > maxSpeed)
+    fourthSpeed = maxSpeed;
 }
